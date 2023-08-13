b83a42a9d382865bb79a9a81d6dca2b1cd8f6015
jairoacuna <jairoja@uninorte.edu.co>
2023-08-13 15:35:18 -0500

    Segundo commit de la actividad

--- diplomado.py
+++ diplomado.py
@@ -11,3 +11,32 @@ productos = [
     {"nombre": "Cinturón de cuero", "precio": 70000, "cantidad_disponible": 35},
     {"nombre": "Zapatos de tacón", "precio": 155000, "cantidad_disponible": 18}
 ]
+
+# Mostrar la lista de productos al usuario
+print("Lista de productos disponibles:")
+for i, producto in enumerate(productos, start=1):
+    print(f"{i}. {producto['nombre']}: ${producto['precio']} (Disponibles: {producto['cantidad_disponible']})")
+
+# Solicitar al usuario que elija productos y cantidades
+carrito = []
+while True:
+    seleccion = input("Seleccione el número de producto que desea comprar (o 'q' para finalizar): ")
+
+    if seleccion == 'q':
+        break
+
+    try:
+        seleccion_num = int(seleccion)
+        if 1 <= seleccion_num <= len(productos):
+            producto_elegido = productos[seleccion_num - 1]
+            cantidad = int(input(f"Ingrese la cantidad de '{producto_elegido['nombre']}' que desea comprar: "))
+            if cantidad <= producto_elegido['cantidad_disponible']:
+                carrito.append({"producto": producto_elegido, "cantidad": cantidad})
+                producto_elegido['cantidad_disponible'] -= cantidad
+                print("Producto agregado al carrito.")
+            else:
+                print("No hay suficiente cantidad disponible.")
+        else:
+            print("Seleccione un número válido.")
+    except ValueError:
+        print("Ingrese un número válido o 'q' para finalizar.")
