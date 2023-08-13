8b81bd17d904f582327f8f61f79b74b67a3ccd93
jairoacuna <jairoja@uninorte.edu.co>
2023-08-13 15:29:46 -0500

    Primer commit de la actividad

---
+++ diplomado.py
@@ -0,0 +1,13 @@
+# Definir una lista de productos con sus nombres, precios y cantidades disponibles
+productos = [
+    {"nombre": "Camiseta básica unisex", "precio": 40000, "cantidad_disponible": 50},
+    {"nombre": "Camisa manga larga hombre", "precio": 85000, "cantidad_disponible": 30},
+    {"nombre": "Blusa de encaje", "precio": 115000, "cantidad_disponible": 25},
+    {"nombre": "Chaqueta de cuero", "precio": 180000, "cantidad_disponible": 20},
+    {"nombre": "Pantalones de lino hombre", "precio": 90000, "cantidad_disponible": 40},
+    {"nombre": "Jeans mujer ajustados", "precio": 120000, "cantidad_disponible": 60},
+    {"nombre": "Jeans hombre ajustados", "precio": 100000, "cantidad_disponible": 15},
+    {"nombre": "Vestido de cóctel", "precio": 250000, "cantidad_disponible": 12},
+    {"nombre": "Cinturón de cuero", "precio": 70000, "cantidad_disponible": 35},
+    {"nombre": "Zapatos de tacón", "precio": 155000, "cantidad_disponible": 18}
+]
